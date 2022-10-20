8448c531f63adde9cc876e7226b0c7f093281368
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-10-20 20:50:16 +0200

    Docker setup

---
+++ rust-backend/tests/tests.rs
@@ -0,0 +1,19 @@
+#[cfg(test)]
+mod tests {
+    use warp::http::StatusCode;
+    use warp::test::request;
+    use super::filters;
+
+    #[tokio::test]
+    async fn try_list() {
+        let api = filters::list();
+
+        let response = request()
+            .method("GET")
+            .path("/plant")
+            .reply(&api)
+            .await;
+
+        assert_eq!(response.status(), StatusCode::ACCEPTED);
+    }
+}
