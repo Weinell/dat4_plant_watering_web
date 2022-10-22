a1a50e0d5d390d34bbf617d1fa49e85a12c68a00
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-10-22 19:23:15 +0200

    Change dockerfile setup to include caching

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -8,8 +8,8 @@ async fn main() {
 
     let routes = health_route.with(warp::cors().allow_any_origin());
 
-    println!("Started server at localhost:8000");
-    warp::serve(routes).run(([0, 0, 0, 0], 8000)).await;
+    println!("Started server at localhost:8080");
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
 }
 
 async fn health_handler() -> Result<impl Reply> {
