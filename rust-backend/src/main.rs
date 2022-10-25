22c06a1a7d7d53fe60bafacae38fad108957887c
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-10-25 19:32:30 +0200

    Back to square one

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -4,9 +4,18 @@ type Result<T> = std::result::Result<T, Rejection>;
 
 #[tokio::main]
 async fn main() {
-    let health_route = warp::path!("health").and_then(health_handler);
 
-    let routes = health_route.with(warp::cors().allow_any_origin());
+    //let root_route = warp::path::end().map(|| "Welcome :)");
+
+    let api_route = warp::path!("api").and_then(health_handler);
+
+    // let second = warp::path!("second").and_then(second_endpoint);
+
+    let routes = api_route.with(warp::cors().allow_any_origin());
+    /*let routes = warp::get()
+    .and(root_route
+        .or(api_route)
+    );*/
 
     println!("Started server at localhost:8080");
     warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
