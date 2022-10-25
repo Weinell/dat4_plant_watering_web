4be52eb5b33d113ce24b7e63e50a41e0d9dd41d5
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-10-26 00:50:59 +0200

    Warp -> Axum

--- rust-backend/src/main.rs
+++ rust-backend/src/main.rs
@@ -1,26 +1,16 @@
-use warp::{Filter, Rejection, Reply};
-
-type Result<T> = std::result::Result<T, Rejection>;
+use axum::{
+    routing::get,
+    Router,
+};
 
 #[tokio::main]
 async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
-    //let root_route = warp::path::end().map(|| "Welcome :)");
-
-    let api_route = warp::path!("api").and_then(health_handler);
-
-    // let second = warp::path!("second").and_then(second_endpoint);
-
-    let routes = api_route.with(warp::cors().allow_any_origin());
-    /*let routes = warp::get()
-    .and(root_route
-        .or(api_route)
-    );*/
-
-    println!("Started server at localhost:8080");
-    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
-}
-
-async fn health_handler() -> Result<impl Reply> {
-    Ok("Healthy!")
+    // run it with hyper on localhost:8000
+    axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
