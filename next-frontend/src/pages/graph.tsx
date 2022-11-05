a4e7b032e42663d5040def2b212a1e13359455e5
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-11-06 00:30:33 +0100

    Add dummy data, legend, tooltip

--- next-frontend/src/pages/graph.tsx
+++ next-frontend/src/pages/graph.tsx
@@ -1,5 +1,5 @@
 import { type NextPage } from "next";
-import { ResponsiveContainer, LineChart, Line, XAxis, YAxis } from 'recharts';
+import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
 import { useEffect, useState } from "react";
 
 
@@ -7,46 +7,57 @@ const data = [
     {
         month: 'January',
         beers: 1,
+        wines: 0,
     },
     {
         month: 'February',
         beers: 4,
+        wines: 0,
     },
     {
         month: 'March',
         beers: 5,
+        wines: 0,
     },
     {
         month: 'April',
         beers: 8,
+        wines: 0,
     },
     {
         month: 'May',
         beers: 6,
+        wines: 2,
     },
     {
         month: 'June',
         beers: 6,
+        wines: 0,
     },
     {
         month: 'July',
         beers: 22,
+        wines: 4,
     },
     {
         month: 'August',
         beers: 14,
+        wines: 0,
     },
     {
         month: 'September',
         beers: 6,
+        wines: 4,
     },
     {
         month: 'October',
         beers: 10,
+        wines: 1,
     },
     {
         month: 'November',
         beers: 2,
+        wines: 3,
     },
 ];
 
@@ -57,15 +68,20 @@ const Graph: NextPage = () => {
         <div>
             <h1>Graph</h1>
 
-            <ResponsiveContainer width="60%" height="100%">
+
+            <ResponsiveContainer width={1300} height={500}>
                 <LineChart width={500} height={400} data={data}>
                     <XAxis dataKey="month" />
                     <YAxis />
+                    <Tooltip />
+                    <Legend />
                     <Line type="monotone" dataKey="beers" stroke="#8884d8" />
+                    <Line type="monotone" dataKey="wines" stroke="#FF0000" />
                 </LineChart>
             </ResponsiveContainer>
+            </div>
 
-        </div>
+
     )
 }
 
