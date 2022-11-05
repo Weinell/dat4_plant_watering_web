ba6e18fb229f054079d8210751ca1d430529ae7b
Nikolaj Weinell <nikolajweinell@hotmail.com>
2022-11-05 23:07:31 +0100

    Add graph page

---
+++ next-frontend/src/pages/graph.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import { Line } from 'react-chartjs-2';
+
+
+
+
+const Graph: NextPage = () => {
+    return (
+
+        <h1>Graph</h1>
+
+
+
+
+    )
+}
+
+export default Graph
